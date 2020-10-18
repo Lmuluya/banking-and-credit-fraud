bdb716df172964ba32e82dd19d7e3581f5f5c3c1
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-10-18 19:25:58 +0300

    Add files via upload

--- banking_credit_frauds.R
+++ banking_credit_frauds.R
@@ -1,1 +1,15 @@
-# Hello
+# Importing data
+data = read.csv("C:\\Users\\mxp comps\\Desktop\\Educba\\Machine Learning Projects\\Banking and Credit Frauds\\creditloandata.csv")
+
+summary(data)
+
+str(data)
+
+# The data set comprises of of the following variabe 
+# Loan Amount
+# Interest Rate
+# Loan Grade
+# Length in Employment
+# Nature of Home ownership
+# Annual income 
+
