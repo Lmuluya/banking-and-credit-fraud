a68ed873c287524c0b7e28e76a85be479573fbda
Lamech Muluya Kibudde <36754197+Lmuluya@users.noreply.github.com>
2020-10-20 15:40:17 +0300

    Add files via upload

--- banking_credit_frauds.R
+++ banking_credit_frauds.R
@@ -12,4 +12,18 @@ str(data)
 # Length in Employment
 # Nature of Home ownership
 # Annual income 
+# Age
 
+xtabs(~ loan_status-grade, data = data)
+
+data$grade <- factor(data$grade)
+
+# Spliting between test and training dataset
+datasort <- sort(sample(nrow(data),nrow(data)*0.75)) # 75% of the data will be used for trainign and the rest will be used for testing 
+
+train <- data[datasort,]
+test <- data[-datasort,]
+
+# Checking the number of rows for test and train datasets
+nrow(train)
+nrow(test)
